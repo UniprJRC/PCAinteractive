efb55e8c016c7acb8bb54794cd4dfc8c4cb597d4
Marco Riani <mriani@unipr.it>
2026-07-09 15:14:46 +0200

    Added data loading

--- codeFig17to18.m
+++ codeFig17to18.m
@@ -4,6 +4,11 @@
 % The following code generates Figures from 17 to 18.
 
 %% Figure 17-18
+load('citiesItaly2024.mat');
+
+Xsel=citiesItaly2024(:,["Employm" "Protest" "UrbanFra"]);
+
+
 % please note that to obtain the same appearance of Figures 17 and 18 
 % of the paper you should manually rotate the plots. 
 figure
